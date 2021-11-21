a5667027e77ae90b4b45cf829ff78816bd07acd6
hanafathiyah <hanafathiyah9b21@gmail.com>
2021-11-21 21:36:27 +0700

    add goal

---
+++ goal.pl
@@ -0,0 +1,7 @@
+:- include('player.pl').
+
+goal :-
+    player_money(X),
+    X >= 20000,
+    write('Congratulations! You have finally collected 20000 golds!Congratulations! You have finally collected 20000 golds!'),
+    !.
