8989cbfcfbf5ef0a1e374e1a8a4d25f5c1507e0e
hanafathiyah <hanafathiyah9b21@gmail.com>
2021-11-22 15:05:28 +0700

    add marketplace

---
+++ marketplace.pl
@@ -0,0 +1,25 @@
+:- include('player.pl').
+
+market :-
+    write('What do you want to do?\n1. Buy\n2. Sell\n'),
+    !.
+
+buy :-
+    write('What do you want to buy?\n'),
+    write('1. wortel (50 golds)\n'),
+    write('2. kentang (50 golds)\n'),
+    write('3. gandum (50 golds)\n'),
+    write('4. padi (50 golds)\n'),
+    write('5. singkong (50 golds)\n'),
+    write('6. jagung (50 golds)\n'),
+    write('7. ayampetelur (100 golds)\n'),
+    write('8. ayambroiler (100 golds)\n'),
+    write('9. sapi (200 golds)\n'),
+    write('10. sapiperah (250 golds)\n'),
+    write('11. domba (150 golds)\n'),
+    write('12. mas (75 golds)\n'),
+    write('13. lele (75 golds)\n'),
+    write('14. gurame (75 golds)\n'),
+    write('15. mujair (75 golds)\n'),
+    write('16. nila (75 golds)\n'),
+    !.
