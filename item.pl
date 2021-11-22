467cc3d696e441a5bd1d15624bc53aef46799bdc
hanafathiyah <hanafathiyah9b21@gmail.com>
2021-11-22 15:16:13 +0700

    add item, hasnt been finished

---
+++ item.pl
@@ -0,0 +1,6 @@
+/* equipment(nama_alat,untuk_apa,level)*/
+equipment(cangkul,farming,1).
+equipment(pancingan,fishing,1).
+equipment(jala,fishing,1).
+equipment(ember,ranching,1).
+equipment(pisau,ranching,1).
