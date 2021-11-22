721143b9a7f6f0bc1d8e50aa51ef47e2636b3978
hanafathiyah <hanafathiyah9b21@gmail.com>
2021-11-22 14:51:16 +0700

    add fail (hasnt been checked)

--- inventory.pl
+++ inventory.pl
@@ -1,3 +1,23 @@
 /* inventory(jenis,jumlah) */
-
-inventory(biji_jagung,10).
+:- dynamic(inventory/2).
+/* inventory(biji_jagung,10). */
+/*umbi-umbian*/
+inventory(wortel,10).
+inventory(kentang,10).
+inventory(gandum,10).
+inventory(padi,10).
+inventory(singkong,10).
+inventory(jagung,10).
+/*hewan ternak*/
+inventory(ayampetelur,10).
+inventory(ayambroiler,10).
+inventory(sapi,10).
+inventory(sapiperah,10).
+inventory(domba,10).
+/*ikan*/
+inventory(mas,10).
+inventory(lele,10).
+inventory(gurame,10).
+inventory(mujair,10).
+inventory(nila,10).
+    
