27871b44940615154644e438825b101e665f544e
hanafathiyah <hanafathiyah9b21@gmail.com>
2021-11-25 20:52:11 +0700

    completed buy, hasnt been connected with main program

--- marketplace.pl
+++ marketplace.pl
@@ -1,5 +1,13 @@
+:- include('player.pl').
+
 market :-
-    write('What do you want to do?\n1. Buy\n2. Sell\n'),
+    write('What do you want to do?\n1. Buy\n2. Sell\n\n'),
+    write('| ?- '),
+    read(Action),
+    (Action == buy ->
+        buy;
+    Action == sell ->
+        sel, !),
     !.
 
 buy :-
@@ -19,8 +27,96 @@ buy :-
     write('13. catfish (75 golds)\n'),
     write('14. gurame (75 golds)\n'),
     write('15. tilapia (75 golds)\n'),
-    write('16. parrotfish (75 golds)\n'),
+    write('16. parrotfish (75 golds)\n\n'),
+    write('| ?- '),
+    read(Itemnumber),
+    write('How many do you want to buy?\n\n'),
+    write('| ?- '), 
+    read(Qty),
+    (Itemnumber == 1 ->
+        write('You have bought '),
+        write(Qty),
+        write(' carrot.\n'),
+        write('You are charged 50 golds.\n');
+    Itemnumber == 2 ->
+        write('You have bought '),
+        write(Qty),
+        write(' potato.\n'),
+        write('You are charged 50 golds.\n');
+    Itemnumber == 3 ->
+        write('You have bought '),
+        write(Qty),
+        write(' wheat.\n'),
+        write('You are charged 50 golds.\n');
+    Itemnumber == 4 ->
+        write('You have bought '),
+        write(Qty),
+        write(' paddy.\n'),
+        write('You are charged 50 golds.\n');
+    Itemnumber == 5 ->
+        write('You have bought '),
+        write(Qty),
+        write(' cassava.\n'),
+        write('You are charged 50 golds.\n');
+    Itemnumber == 6 ->
+        write('You have bought '),
+        write(Qty),
+        write(' corn.\n'),
+        write('You are charged 50 golds.\n');
+    Itemnumber == 7 ->
+        write('You have bought '),
+        write(Qty),
+        write(' laying_hen.\n'),
+        write('You are charged 100 golds.\n');
+    Itemnumber == 8 ->
+        write('You have bought '),
+        write(Qty),
+        write(' broiler_hen.\n'),
+        write('You are charged 100 golds.\n');
+    Itemnumber == 9 ->
+        write('You have bought '),
+        write(Qty),
+        write(' beef_cattle.\n'),
+        write('You are charged 200 golds.\n');
+    Itemnumber == 10 ->
+        write('You have bought '),
+        write(Qty),
+        write(' dairy_cow.\n'),
+        write('You are charged 250 golds.\n');
+    Itemnumber == 11 ->
+        write('You have bought '),
+        write(Qty),
+        write(' sheep.\n'),
+        write('You are charged 150 golds.\n');
+    Itemnumber == 12 ->
+        write('You have bought '),
+        write(Qty),
+        write(' goldfish.\n'),
+        write('You are charged 75 golds.\n');
+    Itemnumber == 13 ->
+        write('You have bought '),
+        write(Qty),
+        write(' catfish.\n'),
+        write('You are charged 75 golds.\n');
+    Itemnumber == 14 ->
+        write('You have bought '),
+        write(Qty),
+        write(' gurame.\n'),
+        write('You are charged 75 golds.\n');
+    Itemnumber == 15 ->
+        write('You have bought '),
+        write(Qty),
+        write(' tilapia.\n'),
+        write('You are charged 75 golds.\n');
+    Itemnumber == 16 ->
+        write('You have bought '),
+        write(Qty),
+        write(' parrotfish.\n'),
+        write('You are charged 75 golds.\n')
+    ,!),
     !.
+
+
 
 sell :-
     write('What do you want to sell?\n'),
