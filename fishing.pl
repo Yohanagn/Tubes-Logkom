dd197d410849b26c51c4427fe7ceec82761bb3d2
Primesz <primahafiz@gmail.com>
2021-11-26 18:15:37 +0700

    fish udah bisa

--- fishing.pl
+++ fishing.pl
@@ -5,34 +5,25 @@ ikan(gurame).
 ikan(tilapia).
 ikan(parrotfish).
 
-list_ikan([goldfish,catfish,gurame,tilapia,parrotfish]).
-
 /* Deklarasi Rules */
-is_water_nearby(X1,X2,Y1,Y2) :-
-    point(X1,Y1,water);
-    point(X1,Y2,water);
-    point(X2,Y1,water);
-    point(X2,Y2,water).
-
-can_do_fishing :-
+is_water_nearby(X,Y) :-
     X1 is X+1,
     X2 is X-1,
     Y1 is Y+1,
     Y2 is Y-1,
-    is_water_nearby(X1,X2,Y1,Y2).
+    point(X,Y1,water);
+    point(X,Y2,water);
+    point(X1,Y,water);
+    point(X2,Y,water).
 
-get_list(X) :-
-    list_ikan(L),
-    member(X,L).
-
-get_index_val(0,[H|_],X) :- X is H.
-get_index_val(Idx,[_|T],X) :-
-    Idx1 is Idx-1,
-    get_index_val(Idx1,T,X).
+can_do_fishing :-
+    player_position(X,Y),
+    is_water_nearby(X,Y)
+    ,!.
 
 generate_koef(X) :-
     player_level(Level_overall),
-    player_level(fisherman,Level_fisherman),
+    player_levelperspecialty(fisherman,Level_fisherman),
     (player_job(fisherman) ->
     Y is 0.2
     ;
@@ -51,16 +42,29 @@ add_exp_fishing(N) :-
     asserta(player_expperspecialty(fisherman,Y1)),
     !.
 
+get_fish(N,X) :-
+    (N == 0 ->
+        X = goldfish;
+    N == 1 ->
+        X = catfish;
+    N == 2 ->
+        X = gurame;
+    N == 3 ->
+        X = tilapia;
+    N == 4 ->
+        X = parrotfish
+    ),
+    !.
+
 /* Command utama */
 
 fish :-
     can_do_fishing,
-    get_list(L),
     generate_koef(Koef),
-    Max_rand is (Koef*40)//1,
+    Max_rand is round(Koef*40),
     random(0,Max_rand,Random_val),
     (Random_val<5 ->
-    get_index_val(Random_val,L,X),
+    get_fish(Random_val,X),
     add_to_inventory(X),
     write('You got '),
     write(X),
@@ -70,7 +74,7 @@ fish :-
     write('You get 10 xp overall and fishing'),
     nl
     ;
-    write("You didn't get anything"),
+    write('You did not get anything'),
     nl,
     Exp is 5,
     add_exp_fishing(Exp),
