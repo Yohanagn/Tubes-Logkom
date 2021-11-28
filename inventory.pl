3a38b121973dc58ff7b2098be8c9112689d25d31
HanselTanoto <hansel.tanoto05@gmail.com>
2021-11-28 12:35:52 +0700

    tidy up

--- inventory.pl
+++ inventory.pl
@@ -1,40 +1,41 @@
 /* inventory(jenis,jumlah) */
+
+/* Dynamic Variable */
 :- dynamic(inventory/2).
 :- dynamic(inventory_seed/2).
 :- dynamic(capacity_inventory/1).
 :- dynamic(inventoryRanching/2).
 :- dynamic(inventoryHasil/2).
 
-/* inventory(biji_jagung,10). */
-/*umbi-umbian*/
+
+/* Hasil Farming */
 inventory(carrot,10).
 inventory(potato,10).
 inventory(wheat,10).
 inventory(paddy,10).
 inventory(cassava,10).
 inventory(corn,10).
-
-/*ikan*/
+/* Hasil Fishing */
 inventory(goldfish,10).
 inventory(catfish,10).
 inventory(gurame,10).
 inventory(tilapia,10).
 inventory(parrotfish,10).
-
-/*hewan ternak*/
-inventoryRanching(laying_hen,10).
-inventoryRanching(broiler_hen,10).
-inventoryRanching(beef_cattle,10).
-inventoryRanching(dairy_cow,10).
-inventoryRanching(sheep,10).
-
-/*Hasil Ternak*/
+/* Hasil Ranching */
 inventoryHasil(eggs, 0).
 inventoryHasil(milk, 0).
 inventoryHasil(wool, 0).
 inventoryHasil(chicken, 0).
 inventoryHasil(beef, 0).
 
+
+/* Modal untuk Ranching - Hewan Ternak */
+inventoryRanching(laying_hen,10).
+inventoryRanching(broiler_hen,10).
+inventoryRanching(beef_cattle,10).
+inventoryRanching(dairy_cow,10).
+inventoryRanching(sheep,10).
+/* Modal untuk Farming - Seed */
 inventory_seed(carrot,3).
 inventory_seed(potato,3).
 inventory_seed(wheat,3).
@@ -42,15 +43,11 @@ inventory_seed(paddy,3).
 inventory_seed(cassava,3).
 inventory_seed(corn,3).
 
+
 /* Inventory operators */
 
 add_to_inventory(X) :-
-    (inventory(X,Y) ->
-    Y1 is Y+1,
-    retractall(inventory(X,Y)),
-    asserta(inventory(X,Y1));
-    asserta(inventory(X,1))
-    ).
+    add_N_to_inventory(1,X).
 
 add_N_to_inventory(N,X) :-
     (inventory(X,Y) ->
@@ -74,12 +71,7 @@ delete_zero_inventory :-
 /* Inventory seed operators */
 
 add_to_inventory_seed(X) :-
-    (inventory_seed(X,Y) ->
-    Y1 is Y+1,
-    retractall(inventory_seed(X,Y)),
-    asserta(inventory_seed(X,Y1));
-    asserta(inventory_seed(X,1))
-    ).
+    add_N_to_inventory_seed(1,X).
 
 add_N_to_inventory_seed(N,X) :-
     (inventory_seed(X,Y) ->
@@ -140,7 +132,7 @@ displayInventory :-
     forall(inventory(X,Y),writeInventory(X,Y)).
 
 displayInventorySeed :-
-    forall(inventory_seed(X,Y),writeInventory(X,Y)).
+    forall(inventory_seed(X,Y),writeInventory(X,Y), write(' (seed)')).
 
 displayInventoryRanching :-
     forall(inventoryRanching(X,Y),writeInventory(X,Y)).
@@ -149,26 +141,30 @@ displayInventoryHasil :-
     forall(inventoryHasil(X,Y),writeInventory(X,Y)).
 
 writeInventory(X,Y) :-
+    Y \== 0,
     write(Y),
     write(' '),
     write(X),
+    (Y > 1 ->
+        write('s')),
     nl,!.
 
 inventory :-
+    nl,
     write('Your inventory '),
     /*capacity_inventory(X),
-    write(X)*/
+    write(X),*/
     write('/100)\n'),
     displayEquipment,
     displayInventory,
-    write('\n'),
-    write('| ?- '),
-    read(Action),
-    (Action == throwItem ->
-        throwItem,!
-    ).
+    displayInventoryHasil,
+    displayInventorySeed,
+    displayInventoryRanching,
+    write('\n').
 
 throwItem :-
+    nl,
+    inventory,
     write('What do you want to throw?\n'),
     read(Item),
     inventory(Item,X),
